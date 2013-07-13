e4a3b13cb828b06aa120cc77a609c26f76c8d2e3
killinsun <noneck.0311@gmail.com>
2013-07-14 04:48:36 +0900

    ぶらっきぃ氏作成のTOPViewとアドレス帳を連結。 メニューボタンを押す事でタブを追加し、複数タブ追加されないように制御。

---
+++ src/TopView/DotBorder.java
@@ -0,0 +1,30 @@
+package TopView;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicGraphicsUtils;
+
+class DotBorder extends LineBorder {
+	
+	public DotBorder(Color color, int thickness) {
+		super(color, thickness);
+	}
+
+	@Override
+	public boolean isBorderOpaque() {
+		return true;
+	}
+
+	@Override
+	public void paintBorder(Component c, Graphics g, int x, int y, int w, int h) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.translate(x, y);
+		g2.setPaint(getLineColor());
+		BasicGraphicsUtils.drawDashedRect(g2, 0, 0, w, h);
+		g2.translate(-x, -y);
+	}
+}
