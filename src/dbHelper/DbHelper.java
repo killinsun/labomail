5084597d666c3ab4e247e8ad80ba27fd3360c3df
aiya000 <x12j007@chiba-fjb.ac.jp>
2013-07-23 21:04:54 +0900

    データベースコードの修正(dbHelper/DbHelper.java)

--- src/dbHelper/DbHelper.java
+++ src/dbHelper/DbHelper.java
@@ -8,38 +8,51 @@ import java.sql.Statement;
 
 public class DbHelper{
 
-//テーブルの作成
-	static final String createAddressTbl = "" + //createAddrrssTblの中身
+	/* テーブル名 */
+
+	public static final String ADDRESS_TABLE = "addresstable";
+	public static final String MASTER_TABLE = "mastertbl";
+	public static final String MBOX_TABLE = "boxtbl";
+
+
+	/* テーブルの作成 */
+
+	static final String createAddressTbl = //createAddrrssTblの中身
 			"CREATE TABLE addresstable " +
-			"( ID integer primary key," +
-			"NAMEID varchar(20) not null, " +
-			"FURIGANA varchar(40) not null, " +
-			"KUBUN varchar(20) not null, " +
-			"PCMAIL varchar(100), " +
-			"PHONEMAIL varchar(100), " +
-			"TEL varchar(13), " +
-			"MEMO varchar(255), " +
-			"FACEICON varchar(255)" +
+			"(" +
+			"	ID integer primary key," +
+			"	NAMEID varchar(20) not null, " +
+			"	FURIGANA varchar(40) not null, " +
+			"	KUBUN varchar(20) not null, " +
+			"	PCMAIL varchar(100), " +
+			"	PHONEMAIL varchar(100), " +
+			"	TEL varchar(13), " +
+			"	MEMO varchar(255), " +
+			"	FACEICON varchar(255)" +
 			")";
-	
-	static final String createMasuterTbl = "" + //createMasuterTblの中身
-			"CREATE TABLE masutertbl　" +
-			"(ID integer primary key autoincrement," +
-			"MBOXID varchar(1) not null," +
-			"BOxID varchar(1) not null," +
-			"MFROM varchar(100) not null," +
-			"MTO varchar(100) not null," +
-			"SUJECT varchar(255)," +
-			"DATA varchar(255) not null," +
-			"DATE varchar('TIMESTAMP')," +
-			"PATH varchar(255) not null" +
+
+	static final String createMasterTbl = //createMasterTblの中身
+			"CREATE TABLE mastertbl" +
+			"(" +
+			"	ID integer primary key autoincrement," +
+			"	MBOXID varchar(1) not null," +
+			"	BOxID varchar(1) not null," +
+			"	MFROM varchar(100) not null," +
+			"	MTO varchar(100) not null," +
+			"	SUBJECT varchar(255)," +
+			"	DATA varchar(255) not null," +
+			"	DATE timestamp," +
+			"	PATH varchar(255) not null" +
 			")";
-	
-	static final String createBoxTbl = "" + //createBoxTblの中身
+
+	static final String createBoxTbl = //createBoxTblの中身
 			"CREATE TABLE boxtbl" +
-			"(BOXID integer primary key," +
-			"BOX varchar(20)　not null," +
+			"(" +
+			"	BOXID integer primary key," +
+			"	BOX varchar(20) not null" +
 			")";
+
+
 	public DbHelper(){ //DbHelperの中身を指定
 		File f = new File("labomailer.db");//ファイルの使用
 		if(!f.exists()){
@@ -49,7 +62,7 @@ public class DbHelper{
 			Statement stmt = conn.createStatement();
 
 			stmt.execute(createAddressTbl);
-			stmt.execute(createMasuterTbl);
+			stmt.execute(createMasterTbl);
 			stmt.execute(createBoxTbl);
 			conn.close();
 			System.out.println("Database created!");
@@ -60,6 +73,8 @@ public class DbHelper{
 
 		}
 	}
+
+
 	public void execSql(String sql){
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -74,8 +89,5 @@ public class DbHelper{
 		}
 
 	}
-
-
-
 
 }
