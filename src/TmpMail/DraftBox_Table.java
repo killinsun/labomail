f3f62697f50272fcb8564fc576633250327c3798
shigure <shigure@192.168.1.2>
2013-07-31 01:23:44 +0900

    チェックがついたデータのIDを取得できるように

--- src/TmpMail/DraftBox_Table.java
+++ src/TmpMail/DraftBox_Table.java
@@ -7,34 +7,32 @@ import javax.swing.table.DefaultTableModel;
 public class DraftBox_Table extends DefaultTableModel {
 	private static final long serialVersionUID = 1L;
 
-	//Boolean型のリスト
+	// Boolean型のリスト
 	ArrayList<Boolean> cbox = new ArrayList<>();
-	
-	//チェックボックス設置
+
+	// チェックボックス設置
 	public DraftBox_Table(String[] clmTitle) {
-		super(null,clmTitle);
+		super(null, clmTitle);
 	}
-	public boolean isCellEditable(int row,int col){
-		//チェックボックスにチェックされていないなら
-		if(col==0){
-			//trueを返す
+
+	public boolean isCellEditable(int row, int col) {// 列幅の編集設定
+		// チェックボックスにチェックされていないなら
+		if (col == 0) {
+			// trueだから編集不可
 			return true;
-		}else{
-			//でなければfalseを返す
+		} else {
 			return false;
 		}
 	}
-	public Class getColumnClass(int col){
+
+	public Class getColumnClass(int col) {
 		return getValueAt(0, col).getClass();
 	}
-	public void add(String f,String s,String c){
-		Object[] data = {
-				new Boolean(false),
-				f,
-				s,
-				c};
-		//行を追加
+
+	public void add(String f, String s, String c) {
+		Object[] data = { new Boolean(false), f, s, c };
+		// 行を追加
 		this.addRow(data);
-		
+
 	}
 }
